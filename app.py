5d3129e51057cf9571bf41098c0c62734da20889
ISeeJp <62587526+ISeeJp@users.noreply.github.com>
2022-06-14 13:44:05 -0400

    Commit 7//Reset

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask, render_template
 from dash import dcc, html, Input, Output, callback
-from pages import formPY
 
-DEBUG = True
+
 app = Flask(__name__)
-app.config.from_object(__name__)
-app.config['SECRET_KEY'] = '7k926bh6492moep0192b3t67rc510x'
+
 
 @app.route("/")
 def main():
-    return html.Div('Hello')
+    return render_template("index.html")
