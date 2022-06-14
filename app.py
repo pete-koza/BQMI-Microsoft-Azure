0371bf3ad247d102430cead6f3194d3fa3c9b59a
ISeeJp <62587526+ISeeJp@users.noreply.github.com>
2022-06-14 11:59:10 -0400

    Commit 1//Attempt To Add Index.html

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+messages = [{'title': 'Message One',
+             'content': 'Message One Content'},
+            {'title': 'Message Two',
+             'content': 'Message Two Content'}
+            ]
+
 @app.route("/")
-def hello():
-    return "Hello World times 2!\n"
+def index():
+    return render_template('index.html', messages=messages)
