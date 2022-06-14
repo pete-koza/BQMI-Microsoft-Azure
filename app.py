152f0c5f3d748d22528ce55360febd1d82ef6018
ISeeJp <62587526+ISeeJp@users.noreply.github.com>
2022-06-14 12:50:38 -0400

    Commit 5//Adding WTForms

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
+
+DEBUG = True
 app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = '7k926bh6492moep0192b3t67rc510x'
 
 @app.route("/")
 def hello():
+    print(__name__)
     return render_template('index.html')
-
-if __name__ == '__app__':
-    app.run(debug=True)
